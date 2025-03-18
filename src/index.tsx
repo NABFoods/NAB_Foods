dad26390ac56bf8ef32b5de9f759127c3425743a
brianmichaelanderson <brianmichealanderson@yahoo.com>
2025-03-17 18:56:42 -0700

    added adminMenu route to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { store } from './client/store';
 import Home from './client/home/Home';
+import AdminMenu from './client/adminMenu/AdminMenu'
 import App from './client/App';
 
 const root = createRoot(document.getElementById('root')!);
@@ -17,9 +18,8 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/adminMenu' element={<AdminMenu />} />
           <Route path='/app' element={<App />} />
-        </Routes>
-
         {/*<App />*/}
       </BrowserRouter>
     </Provider>
