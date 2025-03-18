b40c6cf76d18b8185ed5ef436070b4a57a22aae9
Clara Regula <clararegula@gmail.com>
2025-03-17 22:37:30 -0400

    Starting homeSlice and adding Fetch to FoodCard tsx to get items from database

--- src/client/home/components/FoodCard.tsx
+++ src/client/home/components/FoodCard.tsx
@@ -1,6 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect} from 'react';
+import { useDispatch } from 'react-redux';
 
 const FoodCard: FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.menu)
+      // dispatch(getFoodInfo(data.menu))
+  })
+      ;
+},[])
+
+
   return (
     <>
       <div className='FoodCard'>
--- src/client/home/homeSlice.ts
+++ src/client/home/homeSlice.ts
@@ -1,20 +1,55 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { Product } from '../../types.ts';
+
+
+// Type for ProductState
+export interface HomeState {
+    // Object w/ keys equal to product id
+    foodCard: { 
+        product_name : string,
+        price: number,
+        sold_out: boolean
+        img_url: string,
+        description: string
+     };
+  }
+  
+  // Initial state for productsSlice
+  const initialState: HomeState = {
+    foodCard: { 
+        product_name:'',
+        price: 0,
+        sold_out: false,
+        img_url: '',
+        description: ''
+    },
+  };
+
+
 
 //Using toolkit's createSlice to create reducers for homeSlice
-// const productsSlice = createSlice({
-//   initialState,
-//   name: 'home',
-//   reducers: {
-//     receivedProducts(state, action: PayloadAction<Product[]>) {
-//       const products = action.payload;
-//       products.forEach((product) => {
-//         state.products[product.id] = product;
-//       });
-//     }, // define reducers here}
-//   },
-// });
+const homeSlice = createSlice({
+  initialState,
+  name: 'home',
+  reducers: {
+    //   useEffect(() => {
+    //     fetch('http://localhost:3000/api')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       console.log(data.menu)
+    //       dispatch(getFoodInfo(data.menu))
+    //   })
+    getFoodInfo: (state, action) => {
+       
+    }
+    // receivedProducts(state, action: PayloadAction<Product[]>) {
+    //   const products = action.payload;
+    //   products.forEach((product) => {
+    //     state.products[product.id] = product;
+    //   });
+    // }, // define reducers here}
+  },
+});
 
-// // extracts & exports action creator (receivedProducts) as a .actions object containing all action creators of productsSlice
-// export const { receivedProducts } = homeSlice.actions;
-// export default homeSlice.reducer;
+// extracts & exports action creator (receivedProducts) as a .actions object containing all action creators of productsSlice
+export const { getFoodInfo } = homeSlice.actions;
+export default homeSlice.reducer;
