c8b354205bc4d0eacb0698315f0730c6b90140af
George German <georgegerman.752@gmail.com>
2025-03-17 18:25:47 -0700

    Added some files to start admin components

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import { createRoot } from 'react-dom/client';
 import Cart from './cart/Cart'
-
+import AdminMenu from './adminMenu/adminMenu'
+import Orders from './order/Orders'
 const App: FC = () => {
   return (
     <div>
       < Cart />
+      <AdminMenu />
+      <Orders/>
       {/* <h1>HELLO THERE!</h1> */}
     </div>
   );
---
+++ src/client/adminMenu/AdminMenu.tsx
@@ -0,0 +1,11 @@
+
+
+export function AdminMenu () {
+    return (
+        <div>
+            <h1>Admin Menu</h1>
+        </div>
+    )
+}
+
+export default AdminMenu;
