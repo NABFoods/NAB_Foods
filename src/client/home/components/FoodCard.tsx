800c24c719027d5b69c86cd4be09f4cf9c24027f
Clara Regula <clararegula@gmail.com>
2025-03-17 21:36:42 -0400

    Pushing up some front end changes- Adding React Router

--- src/client/home/components/FoodCard.tsx
+++ src/client/home/components/FoodCard.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 const FoodCard: FC = () => {
   return (
     <>
-      <div>
+      <div className='FoodCard'>
         <h2>Banana</h2>
         <p> Lorem Ipsum Dolar</p>
       </div>
