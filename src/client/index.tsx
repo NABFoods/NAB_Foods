e08dbb2a973e57f0ad4aafc0bb519bd9155ef6a4
VaseanAnnin <vaseanannin@gmail.com>
2025-03-22 15:01:11 -0400

    pulling changes from clara branch

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -25,7 +25,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <GoogleOAuthProvider clientId={clientId!}>
-        <BrowserRouter>
+        <BrowserRouter>styling
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/orders' element={<Orders />} />
