70509d3f6c3756059e3c8b2ff62085b9817bc9ec
VaseanAnnin <vaseanannin@gmail.com>
2025-03-28 15:43:34 -0400

    Working on a more dynamic cart

--- src/client/cart/Cart.tsx
+++ src/client/cart/Cart.tsx
@@ -85,6 +85,10 @@ export function Cart() {
   };
 
   const checkoutButton = async () => {
+    if (customerInfo.name.trim() === '') {
+      console.error('please provide name');
+      return;
+    }
     const response = await fetch('http://localhost:3000/api/createOrder', {
       method: 'POST',
       headers: header,
@@ -99,151 +103,164 @@ export function Cart() {
   // const checkoutState = useAppSelector((state) => state.cart.checkoutState);
 
   return (
-    <div className='mt-20 h-[calc(100vh-6rem)] md:h-calc(100vh-9rem)] flex flex-col text-[#DB162F] lg:flex-row'>
-      {/* <Link to='/'>
-          <button>BACK</button>
-        </Link> */}
-
-      <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40'>
-        <TaskBar />
-        <div></div>
-        {Object.entries(items).map((item, idx) => (
-          <div
-          // className='h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40'
-          // key={idx}
-          >
-            {/* // Products Container */}
-            <div className='flex items-center justify-between mb-4 overflow-scroll'>
-              <button className='cursor-pointer'>X</button>
-              <img src={icon} alt='' width={100} height={100} />
-              <div>
-                <h1 className='uppercase text-xl font-bold'>{item[0]} </h1>
+    <>
+      <div className='lg:mt-20 h-[calc(100vh-6rem)] md:h-calc(100vh-9rem)] flex flex-col text-[#DB162F] lg:flex-row'>
+        <div className='w-full bg-black p-2 justify-start'>
+          <Link to='/'>
+            <button>{'<BACK'}</button>
+          </Link>
+        </div>
+        <div className='flex flex-row justify-center w-full top-0 fixed hidden'>
+          <p className='bg-red-400 text-white rounded-sm p-2'>
+            Please provide ___
+          </p>
+        </div>
+        <div className='h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40'>
+          {/* <TaskBar /> */}
+          {Object.entries(items).map((item, idx) => (
+            <div
+            // className='h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40'
+            // key={idx}
+            >
+              {/* // Products Container */}
+              <div className='flex items-center justify-between mb-4 overflow-scroll'>
+                <button className='cursor-pointer'>X</button>
+                <img src={icon} alt='' width={100} height={100} />
+                <div>
+                  <h1 className='uppercase text-xl font-bold'>{item[0]} </h1>
+                </div>
+                {/* Price of Individual Item */}
+                <h2 className='font-bold'>{item[1][2]}</h2>
+                <button
+                  className='cursor-pointer'
+                  onClick={() =>
+                    dispatch(
+                      deleteFromCart({
+                        product_name: item[0],
+                        price: item[1][0],
+                      })
+                    )
+                  }
+                >
+                  X
+                </button>
+                <button
+                  className='cursor-pointer'
+                  onClick={() =>
+                    dispatch(
+                      addToCart({ product_name: item[0], price: item[1][0] })
+                    )
+                  }
+                >
+                  +
+                </button>
+                <span>{item[1][0]}</span>
+                <button
+                  className='cursor-pointer'
+                  onClick={() =>
+                    dispatch(
+                      removeFromCart({
+                        product_name: item[0],
+                        price: item[1][0],
+                      })
+                    )
+                  }
+                >
+                  -
+                </button>
               </div>
-              {/* Price of Individual Item */}
-              <h2 className='font-bold'>{item[1][2]}</h2>
+            </div>
+          ))}
+        </div>
+        {/* Payments Container */}
+        <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6'>
+          <div>
+            <div className='flex gap-2'>
               <button
-                className='cursor-pointer'
-                onClick={() =>
-                  dispatch(
-                    deleteFromCart({ product_name: item[0], price: item[1][0] })
-                  )
-                }
+                className='bg-[#DB162F] text-white p-3 rounded-md w-1/2 self-end'
+                onClick={() => setPickup(true)}
               >
-                X
+                PICKUP
               </button>
               <button
-                className='cursor-pointer'
-                onClick={() =>
-                  dispatch(
-                    addToCart({ product_name: item[0], price: item[1][0] })
-                  )
-                }
+                className='bg-[#DB162F] text-white p-3 rounded-md w-1/2 self-end'
+                onClick={() => setPickup(false)}
               >
-                +
-              </button>
-              <span>{item[1][0]}</span>
-              <button
-                className='cursor-pointer'
-                onClick={() =>
-                  dispatch(
-                    removeFromCart({ product_name: item[0], price: item[1][0] })
-                  )
-                }
-              >
-                -
+                DELIVERY
               </button>
             </div>
+            {!pickup && (
+              <div className='flex flex-col'>
+                <h1>Delivery Details</h1>
+                <input
+                  type='text'
+                  className='input-field text-black'
+                  name='name'
+                  placeholder='Name'
+                  onChange={handleCustomerInfo}
+                />
+                <input
+                  type='text'
+                  className='input-field text-black'
+                  name='address'
+                  placeholder='Address'
+                  onChange={handleCustomerInfo}
+                />
+                <input
+                  type='text'
+                  className='input-field text-black'
+                  name='phone'
+                  placeholder='Phone'
+                  onChange={handleCustomerInfo}
+                />
+              </div>
+            )}
+            {pickup && (
+              <div className='flex flex-col'>
+                <h1>Pickup Details</h1>
+                <input
+                  type='text'
+                  className='input-field text-black'
+                  name='name'
+                  placeholder='Name'
+                  onChange={handleCustomerInfo}
+                />
+                <input
+                  type='text'
+                  className='input-field text-black'
+                  name='phone'
+                  placeholder='Phone'
+                  onChange={handleCustomerInfo}
+                />
+              </div>
+            )}
           </div>
-        ))}
-      </div>
-      {/* Payments Container */}
-      <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6'>
-        <div>
-          <div className='flex gap-2'>
-            <button
-              className='bg-[#DB162F] text-white p-3 rounded-md w-1/2 self-end'
-              onClick={() => setPickup(true)}
-            >
-              PICKUP
-            </button>
-            <button
-              className='bg-[#DB162F] text-white p-3 rounded-md w-1/2 self-end'
-              onClick={() => setPickup(false)}
-            >
-              DELIVERY
-            </button>
+          <div className='flex justify-between'>
+            <span>Subtotal({quantity})</span>
+            <span>${currentSubtotal()}</span>
           </div>
-          {!pickup && (
-            <div className='flex flex-col'>
-              <h1>Delivery Details</h1>
-              <input
-                type='text'
-                className='input-field text-black'
-                name='name'
-                placeholder='Name'
-                onChange={handleCustomerInfo}
-              />
-              <input
-                type='text'
-                className='input-field text-black'
-                name='address'
-                placeholder='Address'
-                onChange={handleCustomerInfo}
-              />
-              <input
-                type='text'
-                className='input-field text-black'
-                name='phone'
-                placeholder='Phone'
-                onChange={handleCustomerInfo}
-              />
-            </div>
-          )}
-          {pickup && (
-            <div className='flex flex-col'>
-              <h1>Pickup Details</h1>
-              <input
-                type='text'
-                className='input-field text-black'
-                name='name'
-                placeholder='Name'
-                onChange={handleCustomerInfo}
-              />
-              <input
-                type='text'
-                className='input-field text-black'
-                name='phone'
-                placeholder='Phone'
-                onChange={handleCustomerInfo}
-              />
-            </div>
-          )}
+          <div className='flex justify-between'>
+            <span>Service Cost({quantity})</span>
+            <span>$0.00</span>
+          </div>
+          <div className='flex justify-between'>
+            <span>Delivery Cost({quantity})</span>
+            <span className='text-green-500'>Free</span>
+          </div>
+          <hr className='my-2' />
+          <div className='flex justify-between'>
+            <span>TOTAL (INCL.VAT)({quantity})</span>
+            <span className='font-bold'>${currentSubtotal()}</span>
+          </div>
+          <button
+            onClick={checkoutButton}
+            className='bg-[#DB162F] text-white p-3 rounded-md w-1/2 self-end'
+          >
+            CHECKOUT
+          </button>
         </div>
-        <div className='flex justify-between'>
-          <span>Subtotal({quantity})</span>
-          <span>${currentSubtotal()}</span>
-        </div>
-        <div className='flex justify-between'>
-          <span>Service Cost({quantity})</span>
-          <span>$0.00</span>
-        </div>
-        <div className='flex justify-between'>
-          <span>Delivery Cost({quantity})</span>
-          <span className='text-green-500'>Free</span>
-        </div>
-        <hr className='my-2' />
-        <div className='flex justify-between'>
-          <span>TOTAL (INCL.VAT)({quantity})</span>
-          <span className='font-bold'>${currentSubtotal()}</span>
-        </div>
-        <button
-          onClick={checkoutButton}
-          className='bg-[#DB162F] text-white p-3 rounded-md w-1/2 self-end'
-        >
-          CHECKOUT
-        </button>
       </div>
-    </div>
+    </>
   );
 }
 
